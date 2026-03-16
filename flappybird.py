2dbc2fcefced5c1917c7be2978641b74a0e0c385
Vitor Infantini <vitor.i.lopes@aluno.senai.br>
2026-03-16 01:26:49 -0300

    Refactor Flappy Bird game mechanics and graphics

--- flappybird.py
+++ flappybird.py
@@ -1,33 +1,57 @@
 import pygame
 from pygame.locals import *
+from sys import exit 
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 800
+SPEED = 8
+GRAVITY = 1
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Gemini_Generated_Image_6zkm0p6zkm0p6zkm.png').convert_alpha()
+        super().__init__()
+        
+        self.speed = SPEED
+
+        imagem_original = pygame.image.load('Flappy-Bird-Pixel-Art-Transparent-Images.webp').convert_alpha()
+        
+        self.image = pygame.transform.scale(imagem_original, (108, 80))
+        
         self.rect = self.image.get_rect()
-        print(self.rect)
+
+        self.rect[0] = SCREEN_WIDTH / 3
+        self.rect[1] = SCREEN_HEIGHT / 2
+
     def update(self):
-        pass
+        self.rect[1] += self.speed
+        self.speed += GRAVITY
+
+    def bump(self):
+        self.speed = -SPEED
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-BACKGROUND = pygame.image.load('flappy-bird-background-gecj5m4a9yhhjp87.jpg')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (1280, 720))
+BACKGROUND = pygame.image.load('flappy-bird-background-gecj5m4a9yhhjp87.jpg').convert()
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 bird_group = pygame.sprite.Group()
 bird = Bird()
 bird_group.add(bird)
 
+clock = pygame.time.Clock()
+
 while True:
+    clock.tick(30)
     for event in pygame.event.get():
-        if event.type == quit:
+        if event.type == QUIT:
             pygame.quit()
+            
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                bird.bump()
 
     screen.blit(BACKGROUND, (0, 0))
-
     bird_group.update()
     bird_group.draw(screen)
-
     pygame.display.update()
